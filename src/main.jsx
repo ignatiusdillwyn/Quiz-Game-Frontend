ce99ea58fa457fe987d224e5b801155fedc9da76
ignatius_dillwyn <v.ignatius.dillwyn@adira.co.id>
2026-06-15 10:04:59 +0700

    push

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './App.jsx'
+import './styles/App.css'
+import { RouterProvider } from 'react-router-dom'
+import route from './routers/index'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  
+  return (
+    <>
+      <RouterProvider router={route}></RouterProvider>
+    </>
+  )
+}
+
+export default App
